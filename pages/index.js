5ab53a119e4ccf7978902a6153ef1de60c8870fa
Piotr Filipecki <piotrekfilipecki@gmail.com>
2022-11-30 15:59:41 +0100

    commit test.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import {isMobile} from 'react-device-detect';
+import Protect from "react-app-protect";
+import "react-app-protect/dist/index.css";
 
 
 const VideoTrigger = () => {
@@ -14,6 +16,7 @@ const VideoTrigger = () => {
 
 export default function Home() {
   return (
+    
     <div className={styles.container}>
       <Head>
         <title>LXS Land</title>
@@ -22,19 +25,19 @@ export default function Home() {
       </Head>
 
       
-        {/* <video autoPlay playsInline loop muted className={styles.videobg}>
+        <video autoPlay playsInline loop muted className={styles.videobg}>
           <VideoTrigger />
 
           {!isMobile }
             
-        </video> */}
+        </video>
 
-        {/* <div className={styles.wrapper}>
+        <div className={styles.wrapper}>
           <img className={styles.coverimage} src='https://www.datocms-assets.com/26125/1669814297-lxscover.png' />
           <a href="#">
             <img src="https://www.datocms-assets.com/26125/1669814626-orderbutton1-edited.png" />
           </a>
-        </div> */}
+        </div>
 
     </div>
   )
