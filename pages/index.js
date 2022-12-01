243f1d5537d0705db89a2b58567cf505754b3980
Piotr Filipecki <piotrekfilipecki@gmail.com>
2022-12-01 11:45:17 +0100

    final commit.

--- pages/index.js
+++ pages/index.js
@@ -18,23 +18,29 @@ export default function Home() {
       <Head>
         <title>LXS Land</title>
         <meta name="description" content="LXS Land" />
+        <meta property="og:title" content="LXS Land" />
+<meta property="og:site_name" content="LXS Land" />
+<meta property="og:url" content="lxs.land" />
+<meta property="og:description" content="LXS Land" />
+<meta property="og:type" content="" />
+<meta property="og:image" content="https://www.datocms-assets.com/26125/1669814297-lxscover.png"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       
-        {/* <video autoPlay playsInline loop muted className={styles.videobg}>
+        <video autoPlay playsInline loop muted className={styles.videobg}>
           <VideoTrigger />
 
-          {!isMobile }
-            
-        </video> */}
 
-        {/* <div className={styles.wrapper}>
+            
+        </video>
+
+        <div className={styles.wrapper}>
           <img className={styles.coverimage} src='https://www.datocms-assets.com/26125/1669814297-lxscover.png' />
-          <a href="#">
-            <img src="https://www.datocms-assets.com/26125/1669814626-orderbutton1-edited.png" />
+          <a href="https://muzyka.sklep.pl/lxs" target="_blank" rel="noreferrer">
+            <img src="https://www.datocms-assets.com/26125/1669832896-orderbutton1-edited-edited.png" />
           </a>
-        </div> */}
+        </div>
 
     </div>
   )
